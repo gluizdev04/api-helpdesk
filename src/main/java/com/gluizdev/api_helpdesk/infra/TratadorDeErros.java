01a627a45f41c6f1633e4910cc6be9974ea93997
Luiz Gustavo <gluizdev@gmail.com>
2026-05-26 13:54:01 -0400

    feat: implementa tratador de erro global

---
+++ src/main/java/com/gluizdev/api_helpdesk/infra/TratadorDeErros.java
@@ -0,0 +1,32 @@
+package com.gluizdev.api_helpdesk.infra;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity tratar404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratar400(MethodArgumentNotValidException ex) {
+        var fieldErrors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(fieldErrors.stream().map(DadosErroValidacao::new));
+    }
+
+    public record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError fieldError) {
+            this(
+                    fieldError.getField(),
+                    fieldError.getDefaultMessage()
+            );
+        }
+    }
+}
